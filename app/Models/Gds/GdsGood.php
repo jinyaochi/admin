f5fe2c31943721c74c9bc8f940bd5b601c1e8fdd
duanzhiwei <89340545@qq.com>
2019-09-19 22:27:12 +0800

    df

--- app/Models/Gds/GdsGood.php
+++ app/Models/Gds/GdsGood.php
@@ -14,40 +14,10 @@ use App\Models\System\SysCategory;
 
 class GdsGood extends Model
 {
-    protected $appends = ['pay_name','timer_long','timer_longs','pay_view'];
-    protected $casts = ['intro_images' => 'array'];
-    public function setPriceAttribute($value)
-    {
-        $this->attributes['price'] = $value*100;
-    }
-
-    public function getPriceAttribute($value)
-    {
-        return $value/100;
-    }
-
-    public function getPayNameAttribute()
-    {
-        return $this->pay == 1 ? '免费' : ($this->pay == 2 ? '微信支付' :'积分支付');
-    }
-    public function getPayViewAttribute()
-    {
-        return $this->pay == 1 ? '￥0.00' : ($this->pay == 2 ? '￥'.$this->price :'积分:'.$this->price);
-    }
-
-    public function getTimerLongAttribute(){
-        return $this->timer;
-    }
-    public function getTimerLongsAttribute(){
-        return $this->timer;
-    }
+    protected $appends = [];
 
     public function category(){
         return $this->belongsTo(SysCategory::class,'category_id');
-    }
-
-    public function skus(){
-        return $this->hasMany(GdsSku::class,'goods_id');
     }
 
     public function order(){
